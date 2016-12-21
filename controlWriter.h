e5a3f301974de88c5f9431e13b1721dc39e0e929
Timothy Higinbottom <timhigins@gmail.com>
2016-12-21 08:40:17 -0500

    Update controlWriter.h

--- controlWriter.h
+++ controlWriter.h
@@ -23,6 +23,7 @@ void update(int power, float angle) {
   double delta = time.Now - this.old;
   double normalizedPwr = power / delta
   PowerMapping x = new PowerMapping([normalzedPwr, nPwr > 0 ? 1 : 0, null, null]);
-  this.engine.pwmWrite(normalizedPwr, x);
+  this.engine.pwmWrite(normalizedPwr, pins.ENGINE_PWM);
+  this.steering.write();
   this.old = time.Now();
 }
