e83f709328a959df81517364ea59e45cc453e115
Timothy Higinbottom <timhigins@gmail.com>
2016-12-21 08:38:49 -0500

    Create controlWriter.h

---
+++ controlWriter.h
@@ -0,0 +1,28 @@
+#include <wiringpi.h>
+#include <stdio.h>
+#nclude <time.h>
+
+class InputMapper {
+
+public:
+void init(ControlMapping m);
+void update(int power, float angle);
+void close();
+
+}
+
+void init(ControlMapping m) {
+  for(int i = 0; len(m); i++) {
+    z[i] = m[i].map;
+  }
+  this.engine = wiringPi.init(z[0]);
+  this.steering = wiringPi.init(z[1]);
+}
+
+void update(int power, float angle) {
+  double delta = time.Now - this.old;
+  double normalizedPwr = power / delta
+  PowerMapping x = new PowerMapping([normalzedPwr, nPwr > 0 ? 1 : 0, null, null]);
+  this.engine.pwmWrite(normalizedPwr, x);
+  this.old = time.Now();
+}
